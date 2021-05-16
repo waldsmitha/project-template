e7aa29f5a4f9d4390a19584452ffdbfe54cab43d
Austin Waldsmith <austin.waldsmith@gmail.com>
2021-05-15 22:18:39 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+// If single page application, only need the below
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
+
+app.get('/', (req, res) => {
+  res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail:id', (req, res) => {
+  res.render('pages/detail')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
